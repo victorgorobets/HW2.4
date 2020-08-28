4d143d7a4cb2d6c6f29ad31522983c01f5924768
VGO <vgorobets@yandex.ru>
2020-08-28 21:14:31 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by VICTOR on 28.08.2020.
+//  Copyright © 2020 VICTOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
